2fd0d8b068c9a55e6ec31e35e495204139f9902d
srujanab24 <01fe24bci041@kletech.ac.in>
2026-05-25 07:50:40 +0530

    update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
-
+const API_BASE = "https://employeetrack-production.up.railway.app/api";
 const api = axios.create({
   baseURL: API_BASE,
   headers: { "Content-Type": "application/json" },
